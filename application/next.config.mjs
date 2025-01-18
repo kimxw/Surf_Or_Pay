6e6d2d3df181376833deca220be7d881123dfe34
kimxw <kimaya.wanjari@nuscomputing.com>
2025-01-18 16:36:24 +0800

    fix?

---
+++ application/next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.js
+const nextConfig = {
+    images: {
+      domains: ['assets.aceternity.com'], // Allow external images from this domain
+    },
+  };
+  
+  export default nextConfig;
+  
