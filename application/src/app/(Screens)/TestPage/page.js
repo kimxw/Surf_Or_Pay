f4c5c8744fa07232f9d1de15759f62296e30151a
sannie-beep <81695359+sannie-beep@users.noreply.github.com>
2025-01-19 02:36:55 +0800

    Add App logo

---
+++ application/src/app/(Screens)/TestPage/page.js
@@ -0,0 +1,12 @@
+
+"use client";
+import React, { useState } from "react";
+import Calendar from "@/components/ui/calendar";
+
+export function TestPage() {
+  return (
+    <Calendar />
+  );
+}
+
+export default TestPage;
