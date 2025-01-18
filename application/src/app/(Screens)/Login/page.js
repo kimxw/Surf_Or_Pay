45e9152ffec130659cee25eb0bb48ad1e68f2ce7
kimxw <kimaya.wanjari@nuscomputing.com>
2025-01-18 21:09:37 +0800

    Formatting for sign up and log in

--- application/src/app/(Screens)/Login/page.js
+++ application/src/app/(Screens)/Login/page.js
@@ -5,10 +5,11 @@ import { useRouter } from "next/navigation";
 import { handleSignIn } from "@/app/firebase/auth";
 import { useAuth } from "@/app/contexts/AuthContext";
 
+import '@/styles/fonts.css'; 
 
 export function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black py-10">
+    <div className="flex items-center justify-center min-h-screen bg-[#1e2d4f] py-10">
       <div className="bg-white shadow-lg rounded-lg p-5 w-full max-w-md mx-4" style={{borderRadius:'20px'}}>
         <LoginComponent />
       </div>
@@ -45,8 +46,8 @@ function LoginComponent() {
   return (
     <section className="bg-white grid text-center items-center p-8">
       <div>
-        <h3 className="text-3xl text-gray-800 mb-2">Log in</h3>
-        <p className="text-gray-600 mb-16 text-lg">Enter your email and password to log in</p>
+        <h3 className="lucky-guy text-3xl text-[#273a67] mb-2">log in</h3>
+        <p className="text-gray-600 mb-5 text-base">Enter your email and password to log in</p>
         <form action="#" className="mx-auto max-w-[24rem] text-left">
           <div className="mb-6">
             <label htmlFor="email" className="block font-medium text-gray-900 mb-2">
@@ -59,7 +60,7 @@ function LoginComponent() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="name@gmail.com"
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
             />
           </div>
           <div className="mb-6">
@@ -74,7 +75,7 @@ function LoginComponent() {
                 type={passwordShown ? "text" : "password"}
                 name="password"
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
               />
               <span
                 onClick={togglePasswordVisibility}
@@ -90,11 +91,11 @@ function LoginComponent() {
           )}
           <button
             type="Login"
-            className="w-full bg-black text-white p-3 rounded-lg mt-6 style={on-click: handleSubmit()}"
+            className="w-full bg-[#273a67] text-white p-3 rounded-lg mt-6 style={on-click: handleSubmit()}"
             onClick={handleSubmit}
             disabled={isSigningIn}
             >
-                {isSigningIn ? 'Signing in...' : 'Sign in'}
+                {isSigningIn ? 'Logging in...' : 'Log in'}
             </button>
         </form>
       </div>
--- application/src/app/(Screens)/SignUp/page.js
+++ application/src/app/(Screens)/SignUp/page.js
@@ -6,10 +6,11 @@ import { handleSignUp } from "@/app/firebase/auth";
 import { db } from "@/app/firebase/configuration";
 import { collection, setDoc, doc } from "firebase/firestore";
 
+import '@/styles/fonts.css'; 
 
 export function SignUpPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black py-10">
+    <div className="bg-[#1e2d4f] flex items-center justify-center min-h-screen bg-black py-10">
       <div className="bg-white shadow-lg rounded-lg p-5 w-full max-w-md mx-4" style={{borderRadius:'20px'}}>
         <SignUpComponent />
       </div>
@@ -54,12 +55,26 @@ function SignUpComponent() {
   return (
     <section className="bg-white grid text-center items-center p-8">
       <div>
-        <h3 className="text-3xl text-gray-800 mb-2">Sign Up</h3>
-        <p className="text-gray-600 mb-16 text-lg">Enter your email and password to log in</p>
+        <h3 className="lucky-guy text-3xl text-[#273a67] mb-2">sign up form</h3>
+        <p className="text-gray-600 mb-5 text-base">Enter your email and password to sign up</p>
         <form action="#" className="mx-auto max-w-[24rem] text-left">
+        <div className="mb-1">
+            <label htmlFor="username" className="block font-medium text-gray-900 mb-2">
+              Username
+            </label>
+            <input
+              id="username"
+              type="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            />
+          </div>
           <div className="mb-6">
             <label htmlFor="email" className="block font-medium text-gray-900 mb-2">
-              Your Email
+              Email
             </label>
             <input
               id="email"
@@ -68,7 +83,7 @@ function SignUpComponent() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="name@gmail.com"
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
             />
           </div>
           <div className="mb-6">
@@ -83,7 +98,7 @@ function SignUpComponent() {
                 type={passwordShown ? "text" : "password"}
                 name="password"
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
               />
               <span
                 onClick={togglePasswordVisibility}
@@ -92,20 +107,7 @@ function SignUpComponent() {
                 {passwordShown ? 'Hide' : 'Show'}
               </span>
             </div>
-            <div className="mb-6">
-            <label htmlFor="username" className="block font-medium text-gray-900 mb-2">
-              Your Username
-            </label>
-            <input
-              id="username"
-              type="username"
-              name="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Username"
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+            
           </div>
 
           {errorMessage && (
@@ -113,7 +115,7 @@ function SignUpComponent() {
           )}
           <button
             type="Sign Up"
-            className="w-full bg-black text-white p-3 rounded-lg mt-6 style={on-click: handleSubmit()}"
+            className="w-full bg-[#273a67] text-white p-3 rounded-lg mt-6 style={on-click: handleSubmit()}"
             onClick={handleSubmit}
             disabled={isSigningUp}
             >
