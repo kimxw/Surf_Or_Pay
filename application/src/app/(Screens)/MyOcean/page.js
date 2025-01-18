577da0e489fe90a69df23efbd97c84dbb47e1075
kimxw <kimaya.wanjari@nuscomputing.com>
2025-01-18 20:37:31 +0800

    Some changes

--- application/src/app/(Screens)/MyOcean/page.js
+++ application/src/app/(Screens)/MyOcean/page.js
@@ -151,12 +151,12 @@ export const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1 flex-col">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
+    <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full bg-[url('/Background.png')]" style={{ backgroundSize: '102%' }}>
+      <div className="flex gap-2">
           {[...new Array(4)].map((_, idx) => (
             <div
               key={`first-array-${idx}`} // Use `idx` to generate a unique key
-              className="h-20 w-full rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
+              className="h-20 w-full rounded-lg bg-gray-100 opacity-40"
             ></div>
           ))}
         </div>
@@ -164,7 +164,7 @@ const Dashboard = () => {
           {[...new Array(2)].map((_, idx) => (
             <div
               key={`second-array-${idx}`} // Use `idx` to generate a unique key
-              className="h-full w-full rounded-lg bg-gray-100 dark:bg-neutral-800 animate-pulse"
+              className="h-full w-full rounded-lg bg-gray-100 opacity-40"
             ></div>
           ))}
         </div>
