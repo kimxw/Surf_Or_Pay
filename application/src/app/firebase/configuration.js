06e284d1081afa72321b70669dea02ecbafab3a6
kimxw <kimaya.wanjari@nuscomputing.com>
2025-01-19 07:36:47 +0800

    hoping production works

--- application/src/app/firebase/configuration.js
+++ application/src/app/firebase/configuration.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getStorage} from "firebase/storage";
-import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import {collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDG-9TtjXpRzNLpz_fjgo9dQEEGyhTBSkE",
   authDomain: "surforpay.firebaseapp.com",
   projectId: "surforpay",
-  storageBucket: "surforpay.firebasestorage.app",
+  storageBucket: "surforpay.appspot.com", // Updated to correct bucket
   messagingSenderId: "277293475799",
   appId: "1:277293475799:web:7e69d4d73ee71248544d92",
   measurementId: "G-2TC30F5YHL"
@@ -22,6 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const authentication = getAuth(app);
-const db = getFirestore(app); //database
+const db = getFirestore(app); // database
+const storage = getStorage(app); // Firebase storage
 
-export { authentication, db, getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, getStorage,  sendPasswordResetEmail  };
+// Exporting necessary Firebase functions
+export { authentication, db, getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, storage, sendPasswordResetEmail };
