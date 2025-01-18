b09b04cd0044f436fb515fd036e287c3d7173c14
SahanaAvantika <sahanaavantika@gmail.com>
2025-01-18 21:22:46 +0800

    database till Surfer

--- application/src/app/contexts/SurferContext.js
+++ application/src/app/contexts/SurferContext.js
@@ -1,8 +1,85 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { collection, query, where, onSnapshot, updateDoc, deleteDoc, doc, orderBy, increment, getDoc, getDocs } from "firebase/firestore";
+import { collection, query, where, onSnapshot, updateDoc, deleteDoc, doc, orderBy, getDoc, getDocs, addDoc } from "firebase/firestore";
 import { db } from '../firebase/config';
 import { useAuth } from './AuthContext';
 
-const SurferProvider = ({ children }) => {
-    const[tasks, setTasks] = React.useState([]);
-}
+const SurferContext = createContext();
+
+export const useSurfer = () => useContext(SurferContext);
+
+export const SurferProvider = ({ children }) => {
+    const [task, setTask] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const { loggedInUser } = useAuth();
+  
+    useEffect(() => {
+        if (!loggedInUser?.email) return;
+
+        const fetchTasks = async() => {
+            try {
+                const q = query(collection(db, "Surfer"), where("email", "==", loggedInUser?.email),orderBy("deadline", "asc"));
+                const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                    const taskData = [];
+                    querySnapshot.forEach((doc) => {
+                      const data = doc.data();
+                      taskData.push({
+                        id: doc.id,
+                        friend: data.friend,
+                        desc: data.desc, 
+                        credits: data.credits, 
+                        deadline: data.deadline.toDate(),
+                        status: "uncompleted"
+                      });
+                    });
+                    setTask(taskData);
+                    setLoading(false);
+                });
+
+                return unsubscribe;
+            } catch (error) {
+                console.error("Error fetching friends:", error);
+            }
+        };
+
+        const unsubscribe = fetchTasks();
+        return () => unsubscribe && unsubscribe();
+    }, [loggedInUser]);
+
+    const handleAddTasks = async (friendEmail, description, credits, deadline) => {
+        const { loggedInUser } = useAuth();
+    
+        try{
+            await addDoc(collection(db, "Surfer"), {
+              email: loggedInUser?.email,
+              friend: friendEmail,
+              desc: description, 
+              credits: credits, 
+              deadline: deadline,
+              status: "uncompleted"
+            });
+        
+            console.log("Task added successfully!");
+        } catch (error) {
+            console.error("Error adding task:", error);
+        }
+    };
+    
+    const deleteTask = async (taskId) => {
+        const taskDoc = doc(db, "Surfer", taskId);
+        await deleteDoc(taskDoc);
+    };
+
+    const completedTask = async(taskId) => {
+        const taskDoc = doc(db, "Surfer", taskId);
+        await updateDoc(taskDoc, {
+            status: "completed",
+        });
+    }
+
+    return (
+        <SurferContext.Provider value={{ task, loading, handleAddTasks, deleteTask, completedTask }}>
+          {children}
+        </SurferContext.Provider>
+      );
+
+};
