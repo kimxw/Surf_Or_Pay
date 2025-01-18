806d9f60096724a0897f5486f8086ee8ec232df0
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2025-01-18 22:28:08 +0800

    Combine boxes in My Friends

--- application/src/app/(Screens)/MyOcean/MyFriends/page.js
+++ application/src/app/(Screens)/MyOcean/MyFriends/page.js
@@ -154,15 +154,15 @@ const Dashboard = () => {
     <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 flex flex-col gap-2 flex-1 w-full h-full bg-[url('/Background.png')]"
         style={{ backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="flex gap-2">
-          {[...new Array(4)].map((_, idx) => (
+          {[...new Array(1)].map((_, idx) => (
             <div
               key={`first-array-${idx}`} // Use `idx` to generate a unique key
-              className="h-20 w-full rounded-lg bg-gray-100 opacity-40"
+              className="h-20 w-1/2 rounded-lg bg-gray-100 opacity-40"
             ></div>
           ))}
         </div>
         <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((_, idx) => (
+          {[...new Array(1)].map((_, idx) => (
             <div
               key={`second-array-${idx}`} // Use `idx` to generate a unique key
               className="h-full w-full rounded-lg bg-gray-100 opacity-40"
