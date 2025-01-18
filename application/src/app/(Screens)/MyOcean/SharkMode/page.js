6cc3c2edb4f8d4949aee5a33b9408b7c37691de6
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2025-01-18 18:21:47 +0800

    Add My Ocean icon

--- application/src/app/(Screens)/MyOcean/SharkMode/page.js
+++ application/src/app/(Screens)/MyOcean/SharkMode/page.js
@@ -12,13 +12,20 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
+
 export default function SharMode() {
   const links = [
     {
-      label: "Dashboard",
+      label: "My Ocean",
       href: "#",
       icon: (
-        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <Image
+          src="/icons/MyOceanIcon.png"
+          className="h-18 w-20 flex-shrink-0 rounded-xl"
+          width={50}
+          height={50}
+          alt="Avatar"
+        />
       ),
     },
     {
