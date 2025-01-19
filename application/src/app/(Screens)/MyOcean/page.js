409c001e2d7e9c32483a95b1032b8d479b90430e
sannie-beep <81695359+sannie-beep@users.noreply.github.com>
2025-01-19 12:06:20 +0800

    Add my ocean gif

--- application/src/app/(Screens)/MyOcean/page.js
+++ application/src/app/(Screens)/MyOcean/page.js
@@ -195,35 +195,27 @@ export const LogoIcon = () => {
 
 const Dashboard = () => { 
   return ( 
-    <div className="flex flex-1 flex-col"> 
-    <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 border-neutral-700 flex flex-col gap-2 flex-1 w-full h-full bg-[url('/Background.png')]" 
-        style={{ backgroundSize: 'cover', backgroundPosition: 'center' }}> 
- 
-        <h1 className="lucky-guy text-4xl text-[#29597e] dark:[#29597e]"> 
-          My Ocean 
-        </h1> 
- 
-        <div className="flex gap-2 flex-1"> 
- 
-          {/* Left component */} 
-          <div className="h-50 flex-grow flex-basis[55%] bg-gray-200"> 
-            {/* This is your blank component */} 
-            <p className="text-center text-gray-600">Left Component</p> 
-          </div> 
- 
-          {/* Right component */} 
-          {[...new Array(1)].map((_, idx) => ( 
-            <div 
-              key={`second-array-${idx}`} // Use `idx` to generate a unique key 
-              className="h-20 flex-shrink" 
-            > 
-              {/* <CalendarComponent></CalendarComponent>  */}
-            </div> 
-          ))} 
+    <div className="flex flex-1 flex-col h-screen w-screen"> 
+      <div 
+        className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 border-neutral-700 flex flex-col gap-2 flex-1 bg-[url('/Background.png')]"
+        style={{ 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center',
+          height: '100%', // Ensure the container takes full height
+          width: '100%', // Ensure the container takes full width
+        }}
+      > 
+        <div className="flex gap-2 flex-1 justify-center items-center"> 
+          {/* Adjusted to center the content */}
+          <img 
+            src="/icons/MyOceanDisplay.gif" 
+            alt="Gif display"
+            className="h-200 w-400 object-contain" // Ensure the image scales to fit
+          />
         </div> 
-    </div> 
- 
+      </div> 
     </div> 
   ); 
 };
+
 
