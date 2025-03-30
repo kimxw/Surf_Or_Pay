c58fcaf8cb3f0f9da5e99a7ceda2efcf1ac56487
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2025-03-31 03:20:50 +0800

    Create frontend design of profile page but only for fullscreen mode

--- application/src/app/(Screens)/MyOcean/Profile/page.js
+++ application/src/app/(Screens)/MyOcean/Profile/page.js
@@ -9,10 +9,8 @@ import { onSnapshot, doc } from "firebase/firestore";
 import { db } from "@/app/firebase/configuration";
 import { useAuth } from "@/app/contexts/AuthContext";
 import '@/styles/fonts.css';
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import Calendar from "../../../components/ui/calendar";
 
-export default function MyOcean() {
+export default function Profile() {
   const { loggedInUser } = useAuth();
   const [username, setUsername] = useState('');
   const [loading, setLoading] = useState(true);
@@ -164,16 +162,16 @@ export default function MyOcean() {
           </div>
         </SidebarBody>
       </Sidebar>
-      <Dashboard />
+      <ProfileDashboard username={username} email={loggedInUser.email} profilePic={"/icons/UserIcon.svg"} tasksCompleted={12} moneyOwed={120} moneyClaimed={120}/>
     </div>
   );
 }
 
-const Dashboard = () => { 
+const ProfileDashboard = ({ username, email, profilePic, tasksCompleted, moneyOwed, moneyClaimed }) => { 
   return ( 
     <div className="flex flex-1 flex-col h-screen w-screen"> 
       <div 
-        className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 border-neutral-700 flex flex-col gap-2 flex-1 bg-[url('/Background.png')]"
+        className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 border-neutral-700 flex flex-col gap-2 flex-1 bg-[url('/ProfilePageBG.svg')]"
         style={{ 
           backgroundSize: 'cover', 
           backgroundPosition: 'center',
@@ -181,23 +179,72 @@ const Dashboard = () => {
           width: '100%', // Ensure the container takes full width
         }}
       > 
-        <h1 className="lucky-guy text-4xl text-[#29597e] mb-3">
-            My Ocean - Dashboard
+        <h1 className="lucky-guy text-6xl text-[#2C2451] mb-3">
+            My Profile
         </h1>
-        <div className="flex gap-2 justify-center items-start"> 
+        <div className="flex gap-2 justify-between items-start h-screen"> 
         
           {/* left container */}
-          <div className="h-95 w-1/2">
-            <Calendar></Calendar>
+          <div className="h-3/4 w-2/3 mr-8 flex flex-row border border-[#2C2451] border-8 rounded-xl overflow-hidden"> 
+            {/* dark purple box */}
+            <div className="h-full w-1/3 bg-[#52386F] flex flex-col justify-start items-center">
+                <div className="h-60 w-60 flex-shrink-0 rounded-full bg-[#2C2451] m-4 justify-center items-start border border-[#2C2451] border-8">
+                    <Image
+                        src={profilePic}
+                        className="h-60 w-60 flex-shrink-0 rounded-full pb-4"
+                        width={50}
+                        height={50}
+                        alt="Avatar"
+                    />
+                </div>
+                <button
+                type="button"
+                className="lucky-guy h-14 w-60 bg-[#2C2451] text-3xl text-[#C2CFE7] px-4 py-2 rounded-lg flex flex-row items-end">
+                     <img
+                        src="/icons/EditIcon.svg"
+                        alt="EditIcon"
+                        className="h-10 w-auto mr-4"/>
+                    My Avatar
+                </button>    
+            </div>
+            {/* light purple box */}
+            <div className="lucky-guy text-5xl text-[#2C2451] p-8 h-full w-2/3 bg-[#C7CBF5C2]/75">
+                My Info
+                <span className="lucky-guy text-4xl text-[#2B3D67] mt-8 flex flex-col">
+                    Username: 
+                    <span className="font-sans font-bold text-[#345873] text-3xl mb-12 underline">{username}</span>
+
+                    Email: 
+                    <span className="font-sans font-bold text-[#345873] text-3xl mb-12 underline">{email}</span>
+                </span>
+                <button
+                type="button"
+                className="lucky-guy h-14 w-60 bg-[#2C2451] text-3xl text-[#C2CFE7] px-4 py-2 rounded-lg flex justify-center items-end">
+                    Edit Profile
+                </button> 
+            </div>
           </div>
     
           {/* right container */}
-          <div className="h-20 w-1/2 object-cover">
-            <img 
-              src="/icons/MyOceanDisplay.gif" 
-              alt="Gif display"
-              className="h-200 w-400 object-contain" // Ensure the image scales to fit
-            />
+          <div className="h-3/4 w-1/3 lucky-guy text-5xl text-[#3C5182] p-8 object-cover bg-[#97D0FA]/60 rounded-xl flex flex-col justify-start items-center">
+            Analytics
+            <div className="h-2/5 w-full mt-4 mb-12 rounded-xl bg-[#CFE8FB]/50">
+                <span className="lucky-guy text-2xl text-[#2B3D67] m-4 flex flex-col">
+                    Tasks Completed: {tasksCompleted}
+                </span>
+                <span className="lucky-guy text-2xl text-[#6D4339] m-4 flex flex-col">
+                    Money Owed: ${moneyOwed}
+                </span>
+                <span className="lucky-guy text-2xl text-[#2A5842] m-4 flex flex-col">
+                    Money Claimed: ${moneyClaimed}
+                </span>
+            </div>
+            Achievements
+            <div className="h-1/5 w-full mt-4 mb-4 rounded-xl bg-[#CFE8FB]/50">
+                <span className="lucky-guy text-2xl text-[#2B3D67]/70 m-4 flex justify-center items center">
+                    Coming Soon...!
+                </span>
+            </div>
           </div>
         </div> 
       </div> 
